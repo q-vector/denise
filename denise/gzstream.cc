301bdcee3829aa2939b416dabff1e8aeb7342c69
Simon Ching <e.ching@bom.gov.au>
2015-06-26 16:30:02 +0930

    Now really added gzstream

---
+++ denise/gzstream.cc
@@ -0,0 +1,267 @@
+//
+// gzstream.h
+// 
+// Copyright (C) 2005-2015 Simon E. Ching
+// 
+// This file is part of libdenise.
+//
+// libdenise is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libdenise is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libdenise.  If not, see <http://www.gnu.org/licenses/>.
+//
+// gzstream is derived from the Gzstream Library written by Deepak
+// Bandyopadhyay and Lutz Kettner at the Computational Geometry Group
+// at UNC Chapel Hill http://www.cs.unc.edu/Research/compgeom/gzstream/
+
+#include <cstring>
+#include "gzstream.h"
+
+using namespace std;
+using namespace denise;
+
+gzstreambuf::gzstreambuf ()
+   : opened (false)
+{
+
+   char* read_position = buffer + 4;
+   char* end_position = buffer + 4;
+   char* beginning_of_putback_area = buffer + 4;
+
+   setp (buffer, buffer + (buffer_size - 1));
+   setg (beginning_of_putback_area, read_position, end_position);
+
+}
+
+gzstreambuf::~gzstreambuf ()
+{
+   close ();
+}
+       
+bool
+gzstreambuf::is_open ()
+{
+   return opened;
+}
+
+gzstreambuf*
+gzstreambuf::open (const string& file_path,
+                   int open_mode)
+{
+
+   if (is_open ()) { return (gzstreambuf*)0; }
+   this->mode = open_mode;
+
+   const bool is_atend = (mode & std::ios::ate);
+   const bool is_append = (mode & std::ios::app);
+   const bool is_r = (mode & std::ios::in);
+   const bool is_w = (mode & std::ios::out);
+
+   // no atend / append / read-write mode
+   if (is_atend) { return (gzstreambuf*)0; } 
+   if (is_append) { return (gzstreambuf*)0; } 
+   if (is_r && is_w) { return (gzstreambuf*)0; } 
+
+   gz_file = gzopen (file_path.c_str (), is_r ? "rb" : "wb");
+   if (gz_file == 0) { return (gzstreambuf*)0; }
+
+   this->opened = true;
+   return this;
+
+}
+
+gzstreambuf*
+gzstreambuf::close ()
+{
+
+   if (is_open ())
+   {
+      sync ();
+      opened = false;
+      if (gzclose (gz_file) == Z_OK) { return this; }
+   }
+
+   return (gzstreambuf*)0;
+
+}
+
+int
+gzstreambuf::underflow ()
+{
+ 
+   // used for input buffer only
+   if (gptr () && (gptr () < egptr ()))
+   {
+      return * reinterpret_cast<unsigned char *>(gptr ());
+   }
+
+   if (!(mode & std::ios::in) || !opened) { return EOF; }
+
+   const int putback_size = std::min (4, int (gptr () - eback ()));
+
+   char* read_position = buffer + 4;
+   char* beginning_of_putback_area = read_position - putback_size;
+
+   memcpy (beginning_of_putback_area, gptr () - putback_size, putback_size);
+   const int num = gzread (gz_file, buffer + 4, buffer_size - 4);
+
+   // ERROR or EOF
+   if (num <= 0) { return EOF; } 
+
+   // reset buffer pointers
+   char* end_position = read_position + num;
+   setg (beginning_of_putback_area, read_position, end_position);
+
+   // return next character
+   return *reinterpret_cast<unsigned char *>(gptr ());    
+
+}
+
+int
+gzstreambuf::flush_buffer ()
+{
+   // Separate the writing of the buffer from overflow () and
+   // sync () operation.
+   int w = pptr () - pbase ();
+   if (gzwrite (gz_file, pbase (), w) != w) { return EOF; }
+   pbump (-w);
+   return w;
+}
+
+int
+gzstreambuf::overflow (int c)
+{
+
+   if (!(mode & std::ios::out) || ! opened)
+   {
+      return EOF;
+   }
+
+   if (c != EOF)
+   {
+      *pptr () = c;
+      pbump (1);
+   }
+
+   if (flush_buffer () == EOF)
+   {
+      return EOF;
+   }
+
+   return c;
+
+}
+
+int
+gzstreambuf::sync ()
+{
+
+   if (pptr () && pptr () > pbase ())
+   {
+      if (flush_buffer () == EOF) { return -1; }
+   }
+
+   return 0;
+
+}
+
+gzstreambase::gzstreambase ()
+{
+   init (&buf);
+}
+
+gzstreambase::gzstreambase (const string& file_path,
+                            int mode)
+{
+   init (&buf);
+   open (file_path, mode);
+}
+
+gzstreambase::~gzstreambase ()
+{
+   buf.close ();
+}
+
+void
+gzstreambase::open (const string& file_path,
+                    int open_mode)
+{
+   if (!buf.open (file_path, open_mode))
+   {
+      clear (rdstate () | std::ios::badbit);
+   }
+}
+
+void
+gzstreambase::close ()
+{
+   if (buf.is_open () && !buf.close ())
+   {
+      clear (rdstate () | std::ios::badbit);
+   }
+}
+
+gzstreambuf*
+gzstreambase::rdbuf ()
+{
+   return &buf;
+}
+
+igzstream::igzstream ()
+   : std::istream (&buf)
+{
+}
+
+igzstream::igzstream (const string& file_path,
+                      int open_mode)
+   : gzstreambase (file_path, open_mode),
+     std::istream (&buf)
+{
+}
+
+gzstreambuf*
+igzstream::rdbuf ()
+{
+   return gzstreambase::rdbuf ();
+}
+
+void
+igzstream::open (const string& file_path,
+                 int open_mode)
+{
+   gzstreambase::open (file_path, open_mode);
+}
+
+ogzstream::ogzstream ()
+   : std::ostream (&buf)
+{
+}
+
+ogzstream::ogzstream (const string& file_path,
+                      int mode)
+   : gzstreambase (file_path, mode),
+     std::ostream (&buf)
+{
+}
+
+gzstreambuf*
+ogzstream::rdbuf ()
+{
+   return gzstreambase::rdbuf ();
+}
+
+void
+ogzstream::open (const string& file_path,
+                 int open_mode)
+{
+  gzstreambase::open (file_path, open_mode);
+}
+
---
+++ denise/gzstream.h
@@ -0,0 +1,162 @@
+//
+// gzstream.h
+// 
+// Copyright (C) 2005-2015 Simon E. Ching
+// 
+// This file is part of libdenise.
+//
+// libdenise is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libdenise is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libdenise.  If not, see <http://www.gnu.org/licenses/>.
+//
+// gzstream is derived from the Gzstream Library written by Deepak
+// Bandyopadhyay and Lutz Kettner at the Computational Geometry Group
+// at UNC Chapel Hill http://www.cs.unc.edu/Research/compgeom/gzstream/
+
+#ifndef DENISE_GZSTREAM_H
+#define DENISE_GZSTREAM_H
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <zlib.h>
+
+using namespace std;
+
+namespace denise
+{
+
+   class gzstreambuf : public std::streambuf
+   {
+
+      private:
+
+         // totals 512 bytes under g++ for igzstream at the end.
+         // size of data buff
+         static const int
+         buffer_size = 47 + 256;
+
+         gzFile
+         gz_file;
+
+         char
+         buffer[buffer_size];
+
+         bool
+         opened;
+
+         int
+         mode;
+
+         int
+         flush_buffer ();
+
+      public:
+
+         gzstreambuf ();
+
+         bool
+         is_open ();
+
+         gzstreambuf*
+         open (const string& file_path,
+               int open_mode);
+
+         gzstreambuf*
+         close ();
+
+         ~gzstreambuf ();
+
+         virtual int
+         overflow (int c = EOF);
+
+         virtual int
+         underflow ();
+
+         virtual int
+         sync ();
+
+   };
+
+   class gzstreambase : virtual public std::ios
+   {
+
+      protected:
+
+         gzstreambuf
+         buf;
+
+      public:
+
+         gzstreambase ();
+
+         gzstreambase (const string& file_path,
+                       int open_mode);
+
+         ~gzstreambase ();
+
+         void
+         open (const string& file_path,
+               int open_mode);
+
+         void
+         close ();
+
+         gzstreambuf*
+         rdbuf ();
+
+   };
+
+   class igzstream : public gzstreambase,
+                     public std::istream
+   {
+
+      public:
+
+         igzstream ();
+
+         igzstream (const string& file_path,
+                    int open_mode = std::ios::in);
+
+         gzstreambuf*
+         rdbuf ();
+
+         void
+         open (const string& file_path,
+               int open_mode = std::ios::in);
+
+   };
+
+   class ogzstream : public gzstreambase,
+                     public std::ostream
+   {
+
+      public:
+
+         ogzstream ();
+
+         ogzstream (const string& file_path,
+                    int mode = std::ios::out);
+
+         gzstreambuf*
+         rdbuf ();
+
+         void
+         open (const string& file_path,
+               int open_mode = std::ios::out);
+
+   };
+
+};
+
+#endif /* DENISE_GZSTREAM_H */
+
